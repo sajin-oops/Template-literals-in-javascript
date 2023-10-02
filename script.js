de8fb2758e803f9abfc4bbdf85a755a8df1c1653
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-10-02 17:46:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let firstname="Ramya"
+let lastname="Murali"
+let city="Chennai"
+console.log(firstname,lastname,"lives in",city);
+console.log(firstname+" "+lastname +" lives in ",city);
+//backtick
+let mes = `${firstname} ${lastname} lives in ${city}`
+console.log(mes)//Template literals
+
+console.log(`${firstname} ${lastname} lives in ${city}`)
+
+let wish = `Happy 
+Birthday`
+console.log(wish)
+let cat = `my cat's name
+ is "meow"`
+console.log(cat)
+let cats = "my cat`s name is meow"
+console.log(cats)
